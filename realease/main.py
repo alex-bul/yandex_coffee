77680793b53c24a54860c224ea0da50c2b88bd32
alex-bul <50155885+alex-bul@users.noreply.github.com>
2021-02-25 23:09:04 +0200

    Add files via upload
    3

---
+++ realease/main.py
@@ -0,0 +1,74 @@
+import sys
+import sqlite3
+
+from PyQt5 import uic  # Импортируем uic
+from PyQt5.QtSql import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt
+
+from form2 import Ui_Form as form
+from dialog import Ui_Form as dialog
+
+COLUMNS = ["ID", 'название сорта', 'степень обжарки', 'молотый/в зернах', 'описание вкуса', 'цена', 'объем упаковки']
+
+
+class Example(QWidget, form):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.initUI()
+        self.pushButton.clicked.connect(self.openDialog)
+
+    def initUI(self):
+        # Зададим тип базы данных
+        db = QSqlDatabase.addDatabase('QSQLITE')
+        # Укажем имя базы данных
+        db.setDatabaseName('coffee.sqlite3')
+        # И откроем подключение
+        db.open()
+        # QTableView - виджет для отображения данных из базы
+        view = self.tableView
+        # Создадим объект QSqlTableModel,
+        # зададим таблицу, с которой он будет работать,
+        #  и выберем все данные
+        model = QSqlTableModel(self, db)
+        model.setTable('coffee')
+        model.select()
+
+        for i, name in enumerate(COLUMNS):
+            model.setHeaderData(i, Qt.Horizontal, name)
+
+        # Для отображения данных на виджете
+        # свяжем его и нашу модель данных
+        view.setModel(model)
+
+    def openDialog(self):
+        dialog = PreDialog(self)
+        dialog.show()
+
+    def update_sql(self):
+        self.initUI()
+
+
+class PreDialog(QDialog, dialog):
+    def __init__(self, parent=None):  # + parent
+        super(PreDialog, self).__init__(parent)  #
+        self.parent = parent  #
+        self.setupUi(self)
+        conn = sqlite3.connect('coffee.sqlite3')
+        conn.cursor().execute(self.textEdit.toPlainText())
+        conn.commit()
+        self.pushButton.clicked.connect(self.close_d)
+
+    def close_d(self):
+        self.close()
+
+    def closeEvent(self, event):  # +++
+        self.parent.update_sql()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
