2224fe2121bb04a8c0d2661497633256e8054460
alex-bul <50155885+alex-bul@users.noreply.github.com>
2021-02-25 23:02:22 +0200

    Add files via upload
    2

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import sys
+import sqlite3
 
+from PyQt5 import uic  # Импортируем uic
 from PyQt5.QtSql import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import Qt
 
+COLUMNS = ["ID", 'название сорта', 'степень обжарки', 'молотый/в зернах', 'описание вкуса', 'цена', 'объем упаковки']
 
 
 class Example(QWidget):
     def __init__(self):
         super().__init__()
+        uic.loadUi('main.ui', self)
         self.initUI()
+        self.pushButton.clicked.connect(self.openDialog)
 
     def initUI(self):
         # Зададим тип базы данных
@@ -18,10 +23,8 @@ class Example(QWidget):
         db.setDatabaseName('coffee.sqlite3')
         # И откроем подключение
         db.open()
-
-
         # QTableView - виджет для отображения данных из базы
-        view = QTableView(self)
+        view = self.tableView
         # Создадим объект QSqlTableModel,
         # зададим таблицу, с которой он будет работать,
         #  и выберем все данные
@@ -29,18 +32,36 @@ class Example(QWidget):
         model.setTable('coffee')
         model.select()
 
-        for i, name in enumerate(["ID", 'название сорта', 'степень обжарки', 'молотый/в зернах', 'описание вкуса', 'цена', 'объем упаковки']):
+        for i, name in enumerate(COLUMNS):
             model.setHeaderData(i, Qt.Horizontal, name)
-
 
         # Для отображения данных на виджете
         # свяжем его и нашу модель данных
         view.setModel(model)
-        view.move(10, 10)
-        view.resize(617, 315)
 
-        self.setGeometry(300, 100, 650, 450)
-        self.setWindowTitle('Пример работы с QtSql')
+    def openDialog(self):
+        dialog = PreDialog(self)
+        dialog.show()
+
+    def update_sql(self):
+        self.initUI()
+
+
+class PreDialog(QDialog):
+    def __init__(self, parent=None):  # + parent
+        super(PreDialog, self).__init__(parent)  #
+        self.parent = parent  #
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        conn = sqlite3.connect('coffee.sqlite3')
+        conn.cursor().execute(self.textEdit.toPlainText())
+        conn.commit()
+        self.pushButton.clicked.connect(self.close_d)
+
+    def close_d(self):
+        self.close()
+
+    def closeEvent(self, event):  # +++
+        self.parent.update_sql()
 
 
 if __name__ == '__main__':
